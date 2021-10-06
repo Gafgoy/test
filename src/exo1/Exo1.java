3a4285fbb684a8047240cc702902b429887a66c6
hgafs <hgafs@192.168.114.93>
2021-10-06 15:59:56 +0200

    exo1 TP0

---
+++ src/exo1/Exo1.java
@@ -0,0 +1,32 @@
+/*
+*Gafsi Hugo, TDB, 29/09/2021
+*/
+package exo1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author hgafs
+*/
+public class Exo1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        System.out.println("Bonjour");
+        
+        int i=1;
+        
+        String prenom;
+        Scanner sc;
+        sc = new Scanner(System.in);
+        System.out.println("Bonjour, quel est votre prenom ?");
+        prenom = sc.nextLine();
+        
+    }
+    
+}
